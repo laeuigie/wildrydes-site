db6389b16d63ce6dc605e4b4820ca4c226c6834f
Laurel Igiehon <laureligiehon@my.unt.edu>
2023-04-30 19:48:38 -0500

    Testing

--- Unicorn-OG-main/js/temp.js
+++ Unicorn-OG-main/js/temp.js
@@ -1,7 +1,15 @@
 options = {
     position: 'topleft',
-    unit: 'landmiles'
+    unit: 'landmiles',
+    clearMeasurementsOnStop: true,
+    measureControlTitleOn: 'Turn on',
+    measureControlTitleOff: 'Turn off',
+    fixedLine: {
+        color: '#006',
+        weight: 2
+    },
 }
 
 L.control.polylineMeasure(options).addTo(map);
+
 
