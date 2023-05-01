94d5c48a07fbc33c673cf9fe24b5b10fd83c8a3d
Laurel Igiehon <laureligiehon@my.unt.edu>
2023-04-30 23:11:26 -0500

    Testing

--- Unicorn-OG-main/js/esri-map.js
+++ Unicorn-OG-main/js/esri-map.js
@@ -16,7 +16,7 @@ WildRydes.map = WildRydes.map || {};
         let lngInc = (dest.longitude - latlng.lng) / 100;
 
         clearInterval(id);
-        id = setInterval(frame, 5);
+        id = setInterval(frame, 1000);
         function frame() {
             if (tick === 100) {
                 clearInterval(id);
