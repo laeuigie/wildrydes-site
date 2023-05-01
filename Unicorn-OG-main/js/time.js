5e34ebd4e928abcbfab0f53c07772757523e987f
Laurel Igiehon <laureligiehon@my.unt.edu>
2023-04-30 23:44:40 -0500

    Testing

--- Unicorn-OG-main/js/time.js
+++ Unicorn-OG-main/js/time.js
@@ -1,21 +1,58 @@
-function timerStart(duration, display){
-    var timer = duration, seconds;
-    setInterval(function() {
-        seconds = parseInt(timer % 60, 10);
+// function timerStart(duration, display){
+//     var timer = duration, seconds;
+//     setInterval(function() {
+//         seconds = parseInt(timer % 60, 10);
 
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = seconds;
+
+//         if(--timer < 0){
+//             timer = duration;
+//         }
+//     }, 1000);
+
+// }
+
+// window.onload = function() {
+//     var fivesecs = 60 / 12,
+//         display = document.querySelector('time');
+//         startTimer(fivesecs, display);
+// };
+
+function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = seconds;
+        display.textContent = minutes + ":" + seconds; 
 
-        if(--timer < 0){
-            timer = duration;
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
         }
-    }, 1000);
-
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
 }
 
-window.onload = function() {
-    var fivesecs = 60 / 12,
-        display = document.querySelector('time');
-        startTimer(fivesecs, display);
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+
 };
